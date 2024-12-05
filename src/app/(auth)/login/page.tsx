1d7cf0000e72fa50f81d8c05333d679b6096d8ec
MapleSugarstone <147115874+MapleSugarstone@users.noreply.github.com>
2024-12-05 15:55:09 -0800

    Added menu buttons

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,6 @@
 import SignIn from 'views/auth/login';
 
 // ==============================|| PAGE ||==============================
-// Github Test
 
 export default function SignInPage() {
   return <SignIn />;
