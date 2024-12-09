3b97d67659a8ee4ef78a6a32ac3f09172e99100a
CYoung5233 <cyoung17@uw.edu>
2024-12-08 17:27:48 -0800

    Added individual book details route

---
+++ src/app/(dashboard)/book/details/page.tsx
@@ -0,0 +1,10 @@
+import BookDetails from 'views/book/details';
+
+export default function BookInfo() {
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <BookDetails />
+    </div>
+  );
+}
