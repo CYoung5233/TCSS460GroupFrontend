9c7da83f3978d0a612491fa0a238ebe29998ae32
CYoung5233 <cyoung17@uw.edu>
2024-12-08 17:28:06 -0800

    Added message regarding nav bar issues

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -10,7 +10,9 @@ import { NavItemType } from 'types/menu';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems: { items: NavItemType[] } = {
-  items: [samplePage, pages, other, searchBooks], 
+  items: [samplePage, pages, other, searchBooks]
 };
+//This is what's causing the nav bar warning - there is no searchBooks under views/books
+//TODO - either move searchBooks to views/books/ or get rid of the searchBooks item
 
 export default menuItems;
